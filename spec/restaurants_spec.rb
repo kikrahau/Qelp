a6c59c53b6b41ae9ef81f303f28cc0917efda444
kikrahau <vincent.koch@hotmail.com>
2014-09-29 11:26:07 +0100

    install rspec, write first test

---
+++ spec/restaurants_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+# >>> Debugging = save_and_open_page
+
+
+describe 'restaurant' do 
+	context 'without any restaurants created' do 
+		it 'can prompt that there are no restaurants' do
+			visit '/restaurants' 
+			expect(page).to have_content(No restaurants)
+			expect(page).to have_link(Create restaurant)
+		end
+	end
+end
