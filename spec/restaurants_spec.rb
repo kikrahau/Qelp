ef25483b793049116b9bb53d0227cba7a1e54497
kikrahau <vincent.koch@hotmail.com>
2014-09-29 18:00:47 +0100

    Added reviews to restaurant pages

--- spec/restaurants_spec.rb
+++ spec/restaurants_spec.rb
@@ -49,11 +49,22 @@ describe 'restaurant' do
 	context 'restaurant profile page' do
 		before do 
 			@restaurant = Restaurant.create(name: 'KFC', description: 'This is an awesome restaurant', rating: 5)
+			@restaurant.reviews.create(content: "awesome", rating: 5)
+			@restaurant.reviews.create(content: "horrible", rating: 1)
+			@restaurant.reviews.create(content: "mediocre", rating: 3)
+
 		end
 		it 'has its own profile page' do 
 			visit "/restaurants/#{@restaurant.id}"
 			expect(page).to have_content('KFC')
 			expect(page).to have_content('This is an awesome restaurant')
+		end
+
+		it 'has all its reviews printed on its page' do 
+			visit "/restaurants/#{@restaurant.id}"
+			expect(page).to have_content("awesome")
+			expect(page).to have_content("horrible")
+			expect(page).to have_content("mediocre")
 		end
 	end
 
