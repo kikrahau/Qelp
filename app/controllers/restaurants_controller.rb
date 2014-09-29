19db350aabd4dd72d99170cc2483d3e3d3da3de4
kikrahau <vincent.koch@hotmail.com>
2014-09-29 12:04:36 +0100

    test for creating new restaurant passes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,10 @@
 class RestaurantsController < ApplicationController
 
 	def index
+		@restaurants = Restaurant.all
+	end
 
+	def new
+		@restaurant = Restaurant.new
 	end
 end
--- spec/restaurants_spec.rb
+++ spec/restaurants_spec.rb
@@ -11,4 +11,23 @@ describe 'restaurant' do
 			expect(page).to have_link('Create restaurant')
 		end
 	end
+	context 'restaurants have been added' do 
+		before do 
+			Restaurant.create(name: 'Spitzweg', description: 'This is an awesome restaurant', rating: 5)
+		end
+
+		it 'shows one restaurant' do 
+			visit '/restaurants'
+			expect(page). to have_content('Spitzweg')
+			expect(page).not_to have_content('No restaurants')
+		end
+	end
+
+	context 'add restaurants' do 
+		it 'should have a form' do 
+			visit '/restaurants/new'
+			expect(page).to have_css('form')
+		end
+	end
+
 end
